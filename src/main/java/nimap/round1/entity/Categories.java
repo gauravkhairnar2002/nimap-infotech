59f1eb93d769fa07135221087a352e50e1ffe322
Gaurav Khairnar <123081200+gauravkhairnar2002@users.noreply.github.com>
2025-01-20 11:58:39 +0530

    Update Categories.java

--- src/main/java/nimap/round1/entity/Categories.java
+++ src/main/java/nimap/round1/entity/Categories.java
@@ -12,7 +12,6 @@ import java.util.Set;
 public class Categories {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
- // @GeneratedValue(strategy  = GenerationType.IDENTITY)
   @Column(name = "id", nullable = false)
   private Long id;
 
